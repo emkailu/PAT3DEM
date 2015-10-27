b2987e85751f5008362109ddc04ace9f6cf02bea
Kailu Yang <kailu@Kailus-MacBook-Air.local>
2015-10-27 14:13:18 -0500

    v0.1.3

--- bin/p3ctfscreen.py
+++ bin/p3ctfscreen.py
@@ -9,13 +9,13 @@ def main():
 	usage = progname + """ [options] <ctfout.txt>
 	Screen based on the output of p3ctf.py.
 	If there is no '_ctffind3.log', you need to manually move it to bad/ folder.
-	Then use this script to check if the maxres is too low.
+	Then use this script to check if the maxres or the best Thon ring is too low.
 	"""
 	
 	args_def = {'cutoff':10}	
 	parser = argparse.ArgumentParser()
 	parser.add_argument("ctfout", nargs='*', help="specify ctf output.txt to be screened")
-	parser.add_argument("-c", "--cutoff", type=float, help="specify cutoff of maxres, by default {}".format(args_def['cutoff']))
+	parser.add_argument("-c", "--cutoff", type=float, help="specify cutoff of maxres and best ring, by default {}".format(args_def['cutoff']))
 	args = parser.parse_args()
 	
 	if len(sys.argv) == 1:
@@ -29,8 +29,10 @@ def main():
 	# loop over
 	for ctfout in args.ctfout:
 		with open(ctfout) as ctf_read:
-			maxres = float(ctf_read.readlines()[3].replace(';', '').split()[9])
-		if maxres > args.cutoff:
+			lines = ctf_read.readlines()
+		maxres = float(lines[3].replace(';', '').split()[9])
+		best_ring = float(lines[5].split()[6])
+		if maxres > args.cutoff or best_ring > args.cutoff:
 			print ctfout		
 			
 if __name__ == '__main__':

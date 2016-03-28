9f9f73f4bcce925486cfdcead4ae7568f6ec4653
Kailu Yang <emkailu@gmail.com>
2016-03-27 23:07:06 -0500

    v0.2.0

--- bin/p3movie.py
+++ bin/p3movie.py
@@ -14,9 +14,8 @@ def file_base(movie):
 def run_unblur(in_movie, root, com_par):
 	unblur = 'unblur << eof'
 	basename = os.path.basename(os.path.splitext(in_movie)[0])
-	# remove '.p3', '_unfil_movie', if it's in the basename
-	basename = basename.replace('.p3', '')
-	basename = basename.replace('_unfil_movie', '')
+	# remove '.p3', '_unfiltd_movie', if it's in the basename
+	basename = basename.replace('.p3', '').replace('_unfiltd_movie', '')
 	out = basename + '_' + root
 	out_sum = out + '.mrc'
 	out_shift = out + '_shifts.txt'
@@ -24,54 +23,48 @@ def run_unblur(in_movie, root, com_par):
 	com = out + '_unblur.com'
 	log = out + '_unblur.log'
 	expert = 'no'
-	if root == 'unfil':
-		dose_filter = 'no'
-	if com_par['save'] != '0 0 0':
+	if root == 'unfiltd':
 		save_movie = 'yes\n'+out_movie
-	else:
-		save_movie = 'no'			
+		dose_filter = 'no'		
+	elif root == 'filtered':
+		pre_exp = str(float(com_par['dose']) * com_par['first'])
+		dose_filter = '\n'.join(['yes', com_par['dose'], com_par['voltage'], pre_exp])
+		save_movie = 'no'
 	with open(com, 'w') as write_com:
 		write_com.write('\n'.join([unblur, in_movie, str(com_par['nimg']), out_sum, out_shift, com_par['apix'], dose_filter, save_movie, expert, 'eof']))
 	with open(log, 'w') as write_log:
 		subprocess.call(['sh', com], stdout=write_log, stderr=subprocess.STDOUT)
 	return out_movie
 
-def get_ld(in_movie, root, com_par):
-	# get lowdose from _unfil_movie.mrcs
-	basename = in_movie.replace('_unfil_movie.p3.mrcs', '')
-	out = basename + '_' + root
-	ld_movie = out + '_movie.mrcs'
-	ld_sum = out + '.mrc'
-	# lowdose movie
-	n = (com_par['last']-com_par['first']+1)/com_par['avg_bin']
+def get_bin(in_movie, com_par):
+	# get bin from _unfiltd_movie.p3.mrcs
+	bin_movie = in_movie.replace('_movie.p3.mrcs', '_movie.mrcs')
+	n = (com_par['nimg'])/com_par['avg_bin']
 	for i in xrange(n):
 		avg=Averagers.get("mean")
-		n1 = com_par['first']+ i * com_par['avg_bin']
+		n1 = i * com_par['avg_bin']
 		n2 = n1 + com_par['avg_bin']
 		for j in xrange(n1,n2):
 			avg.add_image(EMData(in_movie,j))
-		avg.finish().write_image(ld_movie,i)			
-	# lowdose sum
-	avg=Averagers.get("mean")
-	for i in xrange(n):
-		avg.add_image(EMData(ld_movie,i))
-	avg.finish().write_image(ld_sum)	
-	
+		avg.finish().write_image(bin_movie,i)
+
 def main():
 	progname = os.path.basename(sys.argv[0])
 	usage = progname + """ [options] <movies>
-	Output lowdose/lowdose2 sum/movie.
+	Output unfiltered sum/movie and filtered sum.
 	Needs:
-	unblur (v1.0.2, Grant & Grigorieff, 2015)
-	EMAN2 (v2.12, Tang et al., 2007)
+	'unblur' command (v1.0.2, Grant & Grigorieff, 2015)
+	'EMAN2' python module (v2.11, Tang et al., 2007)
 	"""
 	
-	args_def = {'apix':1.25, 'save':'0 0 0', 'save2':'0 0 0', 'xsuper':7420, 'delete':0}	
+	args_def = {'apix':1.25, 'voltage':200, 'time':200, 'rate':8, 'save':'0 0 0', 'xsuper':7420, 'delete':0}
 	parser = argparse.ArgumentParser()
 	parser.add_argument("movie", nargs='*', help="specify movies to be processed")
 	parser.add_argument("-a", "--apix", type=float, help="specify apix, by default {}".format(args_def['apix']))
-	parser.add_argument("-s", "--save", type=str, help="save a specified number of aligned frames, by default {}, which means do not save. e.g., '2 19 2' means the saved movie starts from frame #2, ends at #19, in total (19-2+1)/2 = 9 frames. if 19 >= the real number of frames of the movie, skip".format(args_def['save']))
-	parser.add_argument("-s2", "--save2", type=str, help="save a second specified number of aligned frames, by default {}, which means do not save. e.g., '2 31 2' means the saved movie starts from frame #2, ends at #31, in total (31-2+1)/2 = 15 frames. if 31 >= the real number of frames of the movie, skip".format(args_def['save2']))
+	parser.add_argument("-v", "--voltage", type=int, help="specify the voltage (kV), by default {}".format(args_def['voltage']))
+	parser.add_argument("-t", "--time", type=float, help="specify exposure time per frame in ms, by default {}".format(args_def['time']))
+	parser.add_argument("-r", "--rate", type=float, help="specify dose rate in e/pix/s (counting pixel, not superresolution), by default {}. if specified as 0, no filtered sum will be output".format(args_def['rate']))
+	parser.add_argument("-s", "--save", type=str, help="save a specified number of aligned frames, by default '{}', which means do not save. e.g., '0 19 4' means the saved movie starts from frame #0, ends at #19, in total (19-0+1)/4 = 5 frames. if 19 >= the real number of frames of the movie, skip".format(args_def['save']))
 	parser.add_argument("-x", "--xsuper", type=int, help="specify the x dimension of superresolution images, by default {}".format(args_def['xsuper']))
 	parser.add_argument("-d", "--delete", type=int, help="delete (!!!) the raw movie (specify as 1), by default {}, which means do not delete".format(args_def['delete']))
 	args = parser.parse_args()
@@ -85,8 +78,11 @@ def main():
 		if args.__dict__[i] == None:
 			args.__dict__[i] = args_def[i]
 	# get common parameters
+	dose = str(args.time/1000.0 * args.rate / args.apix ** 2)
 	apix = str(args.apix)
-	com_par = {'apix':apix, 'save':args.save}
+	voltage = str(args.voltage)
+	first, last, avg_bin = args.save.split()
+	com_par = {'dose':dose, 'apix':apix, 'voltage':voltage, 'save':args.save, 'first':int(first), 'last':int(last), 'avg_bin':int(avg_bin)}
 	# loop over all the input movies
 	for movie in args.movie:
 		movie_raw = movie
@@ -99,45 +95,45 @@ def main():
 			movie, basename = file_base(m_link)			
 		# get nimg
 		com_par['nimg'] = EMUtil.get_image_count(movie)
-		# unify the superresolution to counting	
+		# skip the first few frames, meanwhile, unify the superresolution, or counting (dm4 format) to counting mrcs format
+		if com_par['last'] >= com_par['nimg']:
+			print 'args.save >= the real number of frames of {}, skip'.format(movie_raw)
+			continue
+		if args.save != '0 0 0':
+			com_par['nimg'] = com_par['last']-com_par['first']+1
 		d=EMData(movie,0)
 		if d["nx"] == args.xsuper:
 			m_bin = basename+'_2x.p3.mrcs'
 			for i in xrange(com_par['nimg']):
-				d=EMData(movie, i)
+				d=EMData(movie, i+com_par['first'])
 				d.process_inplace("math.fft.resample",{"n":2})
 				d.write_image(m_bin, i)
 			movie, basename = file_base(m_bin)
-		# unify counting dm4 format to mrcs format
-		if movie[-4:] == '.dm4':
-			m_mrcs = basename+'.p3.mrcs'
+		else:
+			m_mrcs = basename+'_1x.p3.mrcs'
 			for i in xrange(com_par['nimg']):
-				d=EMData(movie, i)
+				d=EMData(movie, i+com_par['first'])
 				d.write_image(m_mrcs, i)
 			movie, basename = file_base(m_mrcs)
 		# link mrcs as mrc format
 		in_movie = basename + '.p3.mrc'
 		os.symlink(movie, in_movie)
-		# unblur: align frames, root = 'unfil'
-		out_movie = run_unblur(in_movie, 'unfil', com_par)
-		# get lowdose
-		first, last, avg_bin = args.save.split()
-		com_par['first'], com_par['last'], com_par['avg_bin'] = int(first), int(last), int(avg_bin)
-		if args.save != '0 0 0' and com_par['last'] < com_par['nimg']:
+		# unblur run 1: align frames, root = 'unfiltd'
+		out_movie = run_unblur(in_movie, 'unfiltd', com_par)
+		# get bin
+		if args.save != '0 0 0':
 			in_movie = out_movie.replace('.mrc', '.mrcs')
 			os.symlink(out_movie, in_movie)			
-			get_ld(in_movie, 'lowdose', com_par)
-			if args.save2 != '0 0 0':
-				first, last, avg_bin = args.save2.split()
-				com_par['first'], com_par['last'], com_par['avg_bin'] = int(first), int(last), int(avg_bin)
-				if com_par['last'] < com_par['nimg']:
-					get_ld(in_movie, 'lowdose2', com_par)
+			get_bin(in_movie, com_par)
+		# unblur run 2: apply dose filter, root = 'filtered'
+		if args.rate != 0:
+			run_unblur(out_movie, 'filtered', com_par)
 		# delete intermediate files, they contain '.p3.'
 		for i in glob.glob(basename_raw + '*.p3.*'):
 			os.unlink(i)
 		# delete the raw movie!!!
 		if args.delete == 1:
 			os.unlink(movie_raw)
-				
+			
 if __name__ == '__main__':
 	main()
--- bin/p3movierun.py
+++ bin/p3movierun.py
@@ -4,22 +4,23 @@ import os
 import sys
 import argparse
 import time
-import pat3dem.cluster as p3c
 
 def main():
 	progname = os.path.basename(sys.argv[0])
 	usage = progname + """ [options] <f.txt>
-	Run p3movie.py to process movies listed in f.txt (except the last one), and the movies will be deleted to save space.
+	Run p3movie.py to process movies listed in f.txt, and the movies will be deleted to save space.
 	"""
 	
-	args_def = {'apix':1.25, 'save':'0 0 0', 'save2':'0 0 0', 'xsuper':7420, 'delete':1}
+	args_def = {'apix':1.25, 'voltage':200, 'time':200, 'rate':8, 'save':'0 0 0', 'xsuper':7420, 'delete':1}
 	parser = argparse.ArgumentParser()
 	parser.add_argument("f", nargs='*', help="specify the txt file used for p3download.py")
 	parser.add_argument("-a", "--apix", type=float, help="specify apix, by default {}".format(args_def['apix']))
-	parser.add_argument("-s", "--save", type=str, help="save a specified number of aligned frames, by default {}, which means do not save. e.g., '2 19 2' means the saved movie starts from frame #2, ends at #19, in total (19-2+1)/2 = 9 frames. if 19 >= the real number of frames of the movie, skip".format(args_def['save']))
-	parser.add_argument("-s2", "--save2", type=str, help="save a second specified number of aligned frames, by default {}, which means do not save. e.g., '2 31 2' means the saved movie starts from frame #2, ends at #31, in total (31-2+1)/2 = 15 frames. if 31 >= the real number of frames of the movie, skip".format(args_def['save2']))
+	parser.add_argument("-v", "--voltage", type=int, help="specify the voltage (kV), by default {}".format(args_def['voltage']))
+	parser.add_argument("-t", "--time", type=float, help="specify exposure time per frame in ms, by default {}".format(args_def['time']))
+	parser.add_argument("-r", "--rate", type=float, help="specify dose rate in e/pix/s (counting pixel, not superresolution), by default {}. if specified as 0, no filtered sum will be output".format(args_def['rate']))
+	parser.add_argument("-s", "--save", type=str, help="save a specified number of aligned frames, by default '{}', which means do not save. e.g., '0 19 4' means the saved movie starts from frame #0, ends at #19, in total (19-0+1)/4 = 5 frames. if 19 >= the real number of frames of the movie, skip".format(args_def['save']))
 	parser.add_argument("-x", "--xsuper", type=int, help="specify the x dimension of superresolution images, by default {}".format(args_def['xsuper']))
-	parser.add_argument("-d", "--delete", type=int, help="delete (!!!) the raw movie (specify as 1), by default {}".format(args_def['delete']))
+	parser.add_argument("-d", "--delete", type=int, help="delete (!!!) the raw movie (specify as 1), by default {}, which means do not delete".format(args_def['delete']))
 	args = parser.parse_args()
 	
 	if len(sys.argv) == 1:
@@ -30,6 +31,8 @@ def main():
 	for i in args_def:
 		if args.__dict__[i] == None:
 			args.__dict__[i] = args_def[i]
+	# import
+	import pat3dem.cluster as p3c
 	# get the local file list based on f.txt
 	f = args.f[0]
 	f2 = f + '.p3movie'
@@ -42,16 +45,29 @@ def main():
 	with open(f2) as f2_r:
 		lines = f2_r.readlines()
 	# run line #i if line #(i+1) exists, the last line will be ignored
+	walltime, cpu, ptile = 1, 1, 1
+	option = "-a {} -v {} -t {} -r {} -s '{}' -x {} -d {}".format(args.apix, args.voltage, args.time, args.rate, args.save, args.xsuper, args.delete)
 	for i, l in enumerate(lines[:-1]):
 		l = l.strip()
 		l2 = lines[i+1].strip()
 		while not os.path.isfile(l2):
 			time.sleep(60)
 		# submit the job, the option '-d 1' means delete the raw movie!!!
+		cmd = "p3movie.py {} {}".format(l, option)
 		basename = os.path.basename(os.path.splitext(l)[0])
-		cmd = "p3movie.py {} -a {} -s '{}' -s2 '{}' -x {} -d {}".format(l, args.apix, args.save, args.save2, args.xsuper, args.delete)
-		walltime, cpu, ptile = 1, 1, 1
 		p3c.ada(cmd, basename, walltime, cpu, ptile)
-				
+	# process the last one
+	last = lines[-1].strip()
+	size = os.path.getsize(last)
+	time.sleep(30)
+	size_new = os.path.getsize(last)
+	while size_new > size:
+		size = size_new
+		time.sleep(30)
+		size_new = os.path.getsize(last)
+	cmd = "p3movie.py {} {}".format(last, option)
+	basename = os.path.basename(os.path.splitext(last)[0])
+	p3c.ada(cmd, basename, walltime, cpu, ptile)	
+		
 if __name__ == '__main__':
 	main()
